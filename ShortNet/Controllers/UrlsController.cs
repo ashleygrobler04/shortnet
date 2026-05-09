3b38b67bb37492f2588d37db6ca9695b8234d9f6
ashleygrobler04 <ashleygrobler04@gmail.com>
2026-05-09 09:09:22 +0200

    * you can create a url  * you can only view the urls that you created and if you're logged in, *you can visit the shortened version of the url you created (needs improvement for now should work) redirects to url, *made footer text be dynamic based on the year for the copyright, *applied migrations

--- ShortNet/Controllers/UrlsController.cs
+++ ShortNet/Controllers/UrlsController.cs
@@ -1,20 +1,58 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ShortNet.Data;
+using ShortNet.Models;
 
-namespace ShortNet.Controllers
+namespace ShortNet.Controllers;
+
+public class UrlsController : Controller
 {
-    public class UrlsController : Controller
+    private ApplicationDbContext _context;
+    public UrlsController(ApplicationDbContext context)
     {
-        // GET: UrlsController
-        [Authorize]
-        public ActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
+    }
 
-public ActionResult Create()
+    // GET: UrlsController
+    [Authorize]
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [Authorize]
+    [HttpPost]
+    public ActionResult Create(Url url)
+    {
+        if (ModelState.IsValid)
         {
-            return View();
+            url.CreatedBy = User.Identity.Name;
+            _context.Urls.Add(url);
+            _context.SaveChanges();
         }
+        return View("Url created!");
+    }
+
+    public ActionResult List()
+    {
+        var userUrls = _context.Urls.Where(u => u.CreatedBy == User.Identity.Name).ToList();
+        ViewData["UserUrls"] = userUrls;
+        return View();
+    }
+
+    [HttpGet("Urls/To/{shortenedUrl}")]
+    public ActionResult To(string shortenedUrl)
+    {
+        var foundUrl = _context.Urls.Where(u => u.ShortUrl == shortenedUrl).FirstOrDefault();
+        if (foundUrl == null)
+        {
+            return View("NotFound");
+        }
+        return Redirect(foundUrl.LongUrl);
     }
 }
